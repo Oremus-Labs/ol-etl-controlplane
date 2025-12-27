dae4208b5b4e25abd863f83a6cdbf142a6088c40
StevenBuglione <stevenbuglione1@gmail.com>
2025-12-26 23:53:25 -0500

    prefect: add index queues 8-11

--- terraform/deployments.tf
+++ terraform/deployments.tf
@@ -241,7 +241,7 @@ locals {
     "process-document-3" = "general-3"
   }
 
-  index_deployment_queues = ["index-0", "index-1", "index-2", "index-3", "index-4", "index-5", "index-6", "index-7"]
+  index_deployment_queues = ["index-0", "index-1", "index-2", "index-3", "index-4", "index-5", "index-6", "index-7", "index-8", "index-9", "index-10", "index-11"]
 }
 
 resource "prefect_deployment" "index_document" {
